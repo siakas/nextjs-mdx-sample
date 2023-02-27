81228ed9cd4d7a1236c6bbf3d3d8f315b72b0fa6
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-02-27 21:00:50 +0900

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import GlobalStyle from '@/styles/global'
+import theme from '@/styles/theme'
+import '@fontsource/inter'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,37 @@
 import type { NextPage } from 'next'
+import { MoonIcon } from '@chakra-ui/icons'
+import { Container, Flex, Heading, IconButton } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <div>Enter</div>
-    </>
+    <Container maxW="container.lg">
+      <Flex
+        as="header"
+        justifyContent="space-between"
+        alignItems="center"
+        py={10}
+      >
+        <Heading as="h1" fontSize="2xl">
+          MdxBlogSample
+        </Heading>
+        <Flex alignItems="center" gap={6} fontWeight={400}>
+          <a href="#">Blog</a>
+          <a href="#">Tags</a>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+          <IconButton
+            aria-label="Toggle Dark Mode"
+            icon={<MoonIcon />}
+            bg="transparent"
+            _hover={{ bg: 'transparent' }}
+          />
+        </Flex>
+      </Flex>
+      <Flex as="footer" alignItems="center" flexDirection="column">
+        <p>Lorem Ipsum • © 2023 • Next.js MDX Blog</p>
+        <p>MDX Nextjs Theme</p>
+      </Flex>
+    </Container>
   )
 }
 
