4ccff0cb77688a95728e75b1094d0639226e8ff6
Sanshiroh SAKAI <sansyo@gmail.com>
2023-03-05 09:02:16 +0900

    不要なパッケージを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,9 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import GlobalStyle from '@/styles/global'
-import theme from '@/styles/theme'
-import '@fontsource/inter'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <GlobalStyle />
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <Component {...pageProps} />
     </>
   )
 }
